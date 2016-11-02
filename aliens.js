187078c4f7a6543c8e24fc6cbcb3766ca812eb36
ageronimo <angelageroniimo@gmail.com>
2016-11-02 08:12:29 -1000

    the aliens are coming!

---
+++ aliens.js
@@ -0,0 +1,18 @@
+var blue = 1000;
+var green = 6000;
+var red = 3000;
+
+if (blue + green + red == 10000){
+  if (green > 5000){
+    console.log("Ugh why are they so ugly??");
+  }
+  if (greed + red > 8000){
+    console.log("It's starting to feel a lot like Christmas.");
+  }
+  if (red + blue <= 3000){
+    console.log("We are sooo dead dude.");
+  }
+  if (red > blue && green > red){
+    console.log("What are we even doing here? We shouldn't have sat on those eggs.")
+  }
+}
